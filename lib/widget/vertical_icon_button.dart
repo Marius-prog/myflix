494102e7b74dc35c972891534feed3f329501b61
Marius-prog <msabaliauskas@gmail.com>
2021-09-09 11:51:02 +0200

    mobile version

---
+++ lib/widget/vertical_icon_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class VerticalIconButton extends StatelessWidget {
+  //const VerticalIconButton({Key? key}) : super(key: key);
+  final IconData icon;
+  final String title;
+  final Function onTap;
+
+  const VerticalIconButton({Key key, this.icon, this.title, this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Icon(icon, color: Colors.white),
+          const SizedBox(height: 2.0),
+          Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
